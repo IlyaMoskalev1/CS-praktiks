b5c4741ccf80f6950418c7c6880da7d4b5c57cde
IlyaMoskalev1 <70806022+IlyaMoskalev1@users.noreply.github.com>
2020-10-10 00:39:16 +0300

    Add files via upload

---
+++ ТМП 1/ТМП 1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ТМП_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Sort sort = new Sort();
+            Queue<string> people = new Queue <string>();
+            people.Enqueue("Кот");
+            people.Enqueue("Миша");
+            people.Enqueue("Филин");
+            people.Enqueue("Жига");
+            people.Enqueue("Мерседес");
+            people.Enqueue("Поэт");
+            foreach(string person in people)
+            {
+                Console.WriteLine(person);
+            }
+            int[] arr = { 1, 3, 9, 12, 2, 4 };
+            int n = arr.Length;
+
+            Sort ob = new Sort();
+            ob.sort(arr);
+
+            Console.WriteLine("Sorted array is");
+            ob.printArray(arr);
+        }
+    }
+}
