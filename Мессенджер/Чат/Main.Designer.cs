d11ec738fd22d92117a0d08c2ae07516da7f8983
IlyaMoskalev1 <70806022+IlyaMoskalev1@users.noreply.github.com>
2020-10-21 22:44:15 +0300

    Add files via upload

---
+++ Мессенджер/Чат/Main.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Чат
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainContainer = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // MainContainer
+            // 
+            this.MainContainer.Location = new System.Drawing.Point(12, 75);
+            this.MainContainer.Name = "MainContainer";
+            this.MainContainer.Size = new System.Drawing.Size(950, 668);
+            this.MainContainer.TabIndex = 0;
+            this.MainContainer.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(974, 755);
+            this.Controls.Add(this.MainContainer);
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Мессенджер";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel MainContainer;
+    }
+}
+
---
+++ Мессенджер/Чат/Main.cs
@@ -0,0 +1,66 @@
+﻿using MaterialSkin;
+using MaterialSkin.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Чат
+{
+    public partial class Main : MaterialForm
+    {
+        private static Main _instance;
+
+        public static Main Instance
+        {
+            get
+            {
+                if (_instance == null)
+                    _instance = new Main();
+                return _instance;
+            }
+        }
+
+
+        public Panel Content 
+            
+            {
+
+            get { return MainContainer; }
+            set { MainContainer = value; }
+            
+            }
+            
+
+
+
+
+        public Main()
+        {
+            InitializeComponent();
+            MaterialSkinManager manager = MaterialSkinManager.Instance;
+            manager.AddFormToManage(this);
+            manager.Theme = MaterialSkinManager.Themes.LIGHT;
+            manager.ColorScheme = new ColorScheme(Primary.Lime400, Primary.Lime500, Primary.Lime500, Accent.Lime200, TextShade.WHITE);
+
+
+
+        }
+
+        private void load_Load(object sender , EventArgs e)
+        {
+            _instance = this;
+            MainContainer.Controls.Add(new UserLogin() { Dock = DockStyle.Fill });
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+    }
+}
